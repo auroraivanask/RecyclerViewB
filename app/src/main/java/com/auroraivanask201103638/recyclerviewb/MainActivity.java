9c5cc889219503dce3d9999eac0c384f8a56f9ee
Eyzaa1 <93369160+Eyzaa1@users.noreply.github.com>
2023-06-14 14:42:32 +0700

    recyclerviewb

--- app/src/main/java/com/auroraivanask201103638/recyclerviewb/MainActivity.java
+++ app/src/main/java/com/auroraivanask201103638/recyclerviewb/MainActivity.java
@@ -4,20 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private FloatingActionButton _addButton;
     private RecyclerView _recyclerView1;
 
     @Override
@@ -27,8 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
         _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
 
+        initAddButton();
+        loadRecyclearView();
+    }
+    private void loadRecyclearView() {
         AsyncHttpClient ahc = new AsyncHttpClient();
-        String url = "https://stmikpontianak.net/011100862/tampilmahasiswa.php";
+        String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
 
         ahc.get(url, new AsyncHttpResponseHandler()
         {
@@ -51,6 +60,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+    }
 
+    private void initAddButton() {
+        _addButton = findViewById(R.id.addButoon);
+
+        _addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddMahasiswaActivity.class);
+                startActivity(intent);
+
+                loadRecyclearView();
+            }
+        });
     }
 }
